c7a900382d91ee2a4f7a87197f8f9442c7dd0abe
Benesh <bennessh@gmail.com>
2023-12-20 23:27:07 +0100

    update dataprovider

--- src/test/java/testsuite/TestCreateEmployee.java
+++ src/test/java/testsuite/TestCreateEmployee.java
@@ -2,13 +2,12 @@ package testsuite;
 import org.opensourcedemo.BaseTest.BaseTest;
 import lombok.extern.log4j.Log4j2;
 import org.opensourcedemo.core.properties_manager.data_manager.Employee;
-import org.opensourcedemo.listerners.Mylisterner;
 import org.opensourcedemo.pagesobjects.LoginPage;
 import org.opensourcedemo.pagesobjects.myinfopage.MyInfoPage;
 import org.opensourcedemo.pagesobjects.pimpages.EmployeeDatailsPage;
 import org.opensourcedemo.pagesobjects.time.ProjectReportSearch;
 import org.testng.Assert;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Log4j2
@@ -16,8 +15,16 @@ public class TestCreateEmployee extends BaseTest {
     public TestCreateEmployee(){
         super("src/main/resources/entry_data/scenario1/config.properties");
     }
-    @Test(testName = "Create Employee PIM" )
-    public void TestCreateEmployeePIM(){
+    @DataProvider(name = "dataCreateEmployee")
+    public Employee[][] dataproviderMethod(){
+        log.info("Load Dayaprovider For Login");
+        Employee[][] data = new Employee[1][2];
+        data[0][0] = configproperties.getEmployee().getFirst();
+        data[0][1] = configproperties.getEmployee().get(1);
+        return data;
+    }
+    @Test(testName = "Create Employee PIM",dataProvider = "dataCreateEmployee")
+    public void TestCreateEmployeePIM( Employee employees1, Employee employe2 ){
         logger = extent.createTest("Create Employee PIM");
         logger.assignAuthor("Ben Omar")
                 .assignCategory("Sanity check");
@@ -25,21 +32,21 @@ public class TestCreateEmployee extends BaseTest {
         String title="PIM";
         //Act
         String titlegetted = new LoginPage(testsetup)
-                .inputUserName(configproperties.getEmployee().get(0).getUser().getUsername())
-                .inputPwd(configproperties.getEmployee().get(0).getUser().getPassword())
+                .inputUserName(employees1.getUser().getUsername())
+                .inputPwd(employees1.getUser().getPassword())
                 .clickButtonLogin()
                 .clickPimPage()
                 .clickAddButton()
-                .typeFirstName(configproperties.getEmployee().get(1).getFirstname())
-                .typeMiddletName(configproperties.getEmployee().get(1).getMiddlename())
-                .typeLastName(configproperties.getEmployee().get(1).getLastname())
+                .typeFirstName(employe2.getFirstname())
+                .typeMiddletName(employe2.getMiddlename())
+                .typeLastName(employe2.getLastname())
                 .clickSaveButton()
                         .clickSaveButton()
                                 .getTitle();
         Assert.assertEquals(titlegetted,title);
     }
-    @Test()//dependsOnMethods = "TestCreateEmployeePIM",testName = "Creation Admin User")
-    public void testCreateUserAdmin(){
+    @Test(dependsOnMethods = "TestCreateEmployeePIM",testName = "Creation Admin User",dataProvider = "dataCreateEmployee")
+    public void testCreateUserAdmin(Employee employees1, Employee employe2){
         logger = extent.createTest("Creation Admin User")
                 .assignAuthor("Ben Omar")
                 .assignCategory("Sanity check")
@@ -48,31 +55,39 @@ public class TestCreateEmployee extends BaseTest {
 
         //Virat  Kohli
         String namelogin = new LoginPage(testsetup)
-                .inputUserName(configproperties.getEmployee().get(0).getUser().getUsername())
-                .inputPwd(configproperties.getEmployee().get(0).getUser().getPassword())
+                .inputUserName(employees1.getUser().getUsername())
+                .inputPwd(employees1.getUser().getPassword())
                 .clickButtonLogin()
                 .clickAdminPage()
                 .clickAddAmdin()
                 .setUserToAdminRole()
-                .typeEmployeeName(configproperties.getEmployee().get(1).getNameComplete())
+                .typeEmployeeName(employe2.getNameComplete())
                 .setSatusUserAccountToEnable()
-                .inputUserNameAdmin(configproperties.getEmployee().get(1).getUser().getUsername())
-                .inputPassword(configproperties.getEmployee().get(1).getUser().getPassword())
-                .inputPassworConfirmation(configproperties.getEmployee().get(1).getUser().getPassword())
+                .inputUserNameAdmin(employe2.getUser().getUsername())
+                .inputPassword(employe2.getUser().getPassword())
+                .inputPassworConfirmation(employe2.getUser().getPassword())
                 .buttonSaveAdmin()
                 .clickToProfil()
                 .logOutbutton()
-                        .inputUserName(configproperties.getEmployee().get(1).getUser().getUsername())
-                                .inputPwd(configproperties.getEmployee().get(1).getUser().getPassword())
+                        .inputUserName(employe2.getUser().getUsername())
+                                .inputPwd(employe2.getUser().getPassword())
                                         .clickButtonLogin()
                 .getNameProfil();
         //Assert
-        Assert.assertEquals(namelogin,configproperties.getEmployee().get(1).getNameWithoutMiddlename());
+        Assert.assertEquals(namelogin,employe2.getNameWithoutMiddlename());
     }
 
+    @DataProvider(name = "dataRemplirFormulaire")
+    public Employee[][] dataProviderMethodFillForm(){
+        log.info("Load Dayaprovider For Login");
+        Employee[][] data = new Employee[1][2];
+        data[0][0] = configproperties.getEmployee().get(1);
+        data[0][1] = configproperties.getEmployee().get(2);
+        return data;
+    }
 
-    @Test(dependsOnMethods = "testCreateUserAdmin",testName = "Remplir le formaulaire ")
-    public void fillLeFormInfo(){
+    @Test(dependsOnMethods = "testCreateUserAdmin",testName = "Remplir le formaulaire ", dataProvider = "dataRemplirFormulaire")
+    public void remplirFormulaire(Employee employe1, Employee employe2){
         logger = extent.createTest("Remplir le formaulaire ")
                 .assignAuthor("Ben Omar")
                 .assignDevice(configproperties.getBrowser().toString())
@@ -82,33 +97,40 @@ public class TestCreateEmployee extends BaseTest {
 
         // Act
         EmployeeDatailsPage pimListPage = new LoginPage(testsetup)
-                .inputUserName(configproperties.getEmployee().get(0).getUser().getUsername())
-                .inputPwd(configproperties.getEmployee().get(0).getUser().getPassword())
+                .inputUserName(employe1.getUser().getUsername())
+                .inputPwd(employe1.getUser().getPassword())
                 .clickButtonLogin()
                 .clickPimPage()
                 .clickAddButton()
-                .typeFirstName(configproperties.getEmployee().get(2).getFirstname())
-                .typeMiddletName(configproperties.getEmployee().get(2).getMiddlename())
-                .typeLastName(configproperties.getEmployee().get(2).getLastname())
+                .typeFirstName(employe2.getFirstname())
+                .typeMiddletName(employe2.getMiddlename())
+                .typeLastName(employe2.getLastname())
                 .clickSaveButton()
-                .typeBirthDate(configproperties.getEmployee().get(2).getBirthdate())
-                .clickGender(configproperties.getEmployee().get(2).getGender())
+                .typeBirthDate(employe2.getBirthdate())
+                .clickGender(employe2.getGender())
                 .clickSaveButton()
                 .clicktBloodListInput()
                 .clickRancdomBloodtypeOption()
                 .clickSaveButtonWithBloodType()
                 .refreshPage();
         //Assert
-        Assert.assertEquals(pimListPage.getBirtdaydate(),configproperties.getEmployee().get(2).getBirthdate());
-        if (configproperties.getEmployee().get(2).getGender().equals("M")){
+        Assert.assertEquals(pimListPage.getBirtdaydate(),employe2.getBirthdate());
+        if (employe2.getGender().equals("M")){
             Assert.assertTrue (pimListPage.isRadioButtonMaleSelected());
         }else {
             Assert.assertTrue (pimListPage.isRadioButtonFemalealeSelected());
         }
         Assert.assertNotNull(pimListPage.getBloodTypeValue());
     }
-
-    @Test(testName = "Feuille de temps",dependsOnMethods = "testCreateUserAdmin")
+    @DataProvider(name = "dataFeuilledeTmps")
+    public Object[][] dataProviderMethodFeuillDetemps(){
+        log.info("Load Dayaprovider For Login");
+        Object[][] data = new Object[1][2];
+        data[0][0] = configproperties.getEmployee().get(1);
+        data[0][1] = configproperties.getEmployee().get(2);
+        return data;
+    }
+    @Test(testName = "Feuille de temps",dependsOnMethods = "testCreateUserAdmin",dataProvider = "dataFeuilledeTmps")
     public void testFeuilledetemps(){
         logger = extent.createTest("Feuille de temps")
                 .assignCategory("Feature Gestion Temps")
